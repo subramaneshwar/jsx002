c227551c44caf5db84d1a133df4a44bafaa6258a
subramaneshwar <subramaneshwar@gmail.com>
2023-02-23 12:23:22 +0530

    commits message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Temp from './comp';
+
+function App() {
+  return (
+    <div className="App">
+      <Temp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp.jsx
@@ -0,0 +1,13 @@
+import './comp.css'
+
+const Temp = () =>{
+    return(
+        <div className="head">
+            <h1>The Evolution of</h1>
+            <h1>Airbnb's Frontend</h1>
+            <p>@spikebrehm</p>
+            <img src={require('./images-removebg-preview.png')}alt="" />
+        </div>
+    )
+}
+export default Temp
